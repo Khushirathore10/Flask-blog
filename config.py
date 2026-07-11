8f3f4102977339d667f5880540223cdea51a05cf
khushirathore10 <khushirathore10062005@gmail.com>
2026-07-11 18:20:08 +0530

    Fix database configuration

--- config.py
+++ config.py
@@ -4,14 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+instance_path = os.path.join(basedir, "instance")
+os.makedirs(instance_path, exist_ok=True)
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = (
         os.getenv("DATABASE_URL")
-        or "sqlite:///" + os.path.join(basedir, "instance", "site.db")
+        or "sqlite:///" + os.path.join(instance_path, "site.db")
     )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
@@ -20,5 +24,3 @@ class Config:
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
